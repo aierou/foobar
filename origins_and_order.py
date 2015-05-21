14e6d4052185d3665c2bef0f6b20abe6ec8938ca
aierou <aierou@live.com>
2015-05-21 15:26:23 -0400

    Added gitignore and other solution

---
+++ origins_and_order.py
@@ -0,0 +1,14 @@
+import itertools, datetime
+
+def getDate(test):
+	try:
+		return datetime.date(*test)
+	except ValueError:
+		return None
+
+def answer(x,y,z):
+	results = filter(lambda a: a != None, map(getDate, set(itertools.permutations([x,y,z]))))
+	if(len(results) > 1): return "Ambiguous"
+	return str(results[0].month).zfill(2) + "/" + str(results[0].day).zfill(2) + "/" + str(results[0].year).zfill(2)
+	
+print answer(2,13,31)
